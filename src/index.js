19ecd4f20c617666d926917ed250f27f3f6a81f6
dev <dev.aravind@culturecreates.com>
2024-10-15 17:10:00 +0530

    Debugging push to artsdata

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
 const axios = require('axios');
-
 
 async function run() {
   let artsDataApiResponse;
   try {
-    const artifact = core.getInput('artifact');
-    const publisher = core.getInput('publisher');
-    const downloadFile = core.getInput('downloadFile');
-    const downloadUrl = "https://raw.githubusercontent.com/" + github.context.repo.owner + "/" + github.context.repo.repo + "/" + github.context.sha + "/output/" + downloadFile;
-    const group = core.getInput('group') || github.context.repo.repo;
-    const version = core.getInput('version') || new Date().toISOString().replace(/:/g, "_")
-    const comment = core.getInput('comment') || `Published by ${group} on ${version}`
+    const artifact = process.env.artifact
+    const publisher = process.env.publisher
+    const downloadFile = process.env.downloadFile
+    
+    const repo = process.env.GITHUB_REPOSITORY;
+    const sha = process.env.GITHUB_SHA;
+    const downloadUrl = `https://raw.githubusercontent.com/${repo}/${sha}/output/${downloadFile}`;
+    
+    const group = process.env.group || repo.split('/')[0];
+    const version = process.env.version || new Date().toISOString().replace(/:/g, "_");
+    const comment = process.env.comment || `Published by ${group} on ${version}`;
 
     // Construct data payload
     const data = {
@@ -25,7 +26,7 @@ async function run() {
       comment: comment
     };
 
-    console.log('Data:', data)
+    console.log('Data:', data);
 
     // Perform HTTP request
     artsDataApiResponse = await axios.post('http://api.artsdata.ca/databus/', data, {
@@ -35,8 +36,7 @@ async function run() {
     });
 
   } catch (error) {
-    console.log('Error:', error.response?.data)
-    core.setFailed(error.message);
+    console.log('Error:', error.response?.data);
   }
 }
 
