236cd618c9410bec64d8134a5ad7028d11a20e93
dev <dev.aravind@culturecreates.com>
2024-11-08 15:49:54 +0530

    Updated docker image version to latest

--- src/lib/sparql_processor.rb
+++ src/lib/sparql_processor.rb
@@ -1,6 +1,7 @@
 module SparqlProcessor
   def self.perform_sparql_transformations(graph, sparql_paths, base_url)
     sparql_paths.each do |sparql_path|
+      puts "Executing #{sparql_path}"
       file = File.read(sparql_path).gsub("domain_name", base_url)
       graph.query(SPARQL.parse(file, update: true))
     end
