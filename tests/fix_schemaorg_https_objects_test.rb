810c56e9d271abd82ab335bf7a25846279e14c90
dev <dev.aravind@culturecreates.com>
2024-12-03 12:40:41 +0530

    Added SPARQL transformations for gtq and unit tests

---
+++ tests/fix_schemaorg_https_objects_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'linkeddata'
+
+class FixSchemaOrgHttpsObjectsTest < Minitest::Test
+  def setup
+    @sparql_file = "./sparql/fix_schemaorg_https_objects.sparql"
+  end
+
+  def test_fix_schemaorg_https_objects
+    sparql = SPARQL.parse(File.read(@sparql_file), update: true)
+    graph = RDF::Graph.load("./tests/fixtures/test_fix_schemaorg_https_objects.jsonld")
+    graph.query(sparql)
+    assert_equal(
+      RDF::URI("http://schema.org/Example1"),
+      graph.query([RDF::URI("http://example.org/resource1"), RDF::URI("http://schema.org/someProperty"), nil]).objects.first
+    )
+  end
+end
---
+++ tests/fix_wikidata_uri_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'linkeddata'
+
+class FixWikidataUriTest < Minitest::Test
+  def setup
+    @sparql_file = "./sparql/fix_wikidata_uri.sparql"
+  end
+
+  def test_fix_wikidata_uri
+    sparql = SPARQL.parse(File.read(@sparql_file), update: true)
+    graph = RDF::Graph.load("./tests/fixtures/test_fix_wikidata_uri.jsonld")
+    graph.query(sparql)
+    assert_equal(
+      RDF::URI("http://www.wikidata.org/entity/Q42"),
+      graph.query([RDF::URI("http://example.com/entity/123"), RDF::URI("http://schema.org/sameAs"), nil]).objects.first
+    )
+  end
+end
