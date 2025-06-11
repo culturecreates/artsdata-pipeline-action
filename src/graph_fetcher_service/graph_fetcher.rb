2cde73311dcbcac4ddd97ece81ad5be6a3ef9fd7
dev <dev.aravind@culturecreates.com>
2025-06-11 12:28:26 +0530

    enhancement(uri-fetcher): added xpath support, increased SPARQL efficiency

--- src/graph_fetcher_service/graph_fetcher.rb
+++ src/graph_fetcher_service/graph_fetcher.rb
@@ -24,16 +24,16 @@ module GraphFetcherService
         end
         loaded_graph = @sparql.perform_sparql_transformation(loaded_graph, 'add_derived_from.sparql', 'subject_url', entity_url)
         loaded_graph = @sparql.perform_sparql_transformation(loaded_graph, 'add_language.sparql', 'subject_url', entity_url)
-        
+        loaded_graph = @sparql.perform_sparql_transformation(loaded_graph, "remove_objects.sparql")
+        loaded_graph = @sparql.perform_sparql_transformation(loaded_graph, "replace_blank_nodes.sparql", "domain_name", entity_urls[0].split('/')[0..2].join('/'))
+        loaded_graph = @sparql.perform_sparql_transformation(loaded_graph, "fix_date_timezone.sparql")
+        loaded_graph = @sparql.perform_sparql_transformation(loaded_graph, "fix_schemaorg_https_objects.sparql")
+
         graph << loaded_graph
       end
-      graph = @sparql.perform_sparql_transformation(graph, "remove_objects.sparql")
-      graph = @sparql.perform_sparql_transformation(graph, "replace_blank_nodes.sparql", "domain_name", entity_urls[0].split('/')[0..2].join('/'))
       graph = @sparql.perform_sparql_transformation(graph, "fix_entity_type_capital.sparql")
-      graph = @sparql.perform_sparql_transformation(graph, "fix_date_timezone.sparql")
       graph = @sparql.perform_sparql_transformation(graph, "fix_address_country_name.sparql")
       graph = @sparql.perform_sparql_transformation(graph, "fix_malformed_urls.sparql")
-      graph = @sparql.perform_sparql_transformation(graph, "fix_schemaorg_https_objects.sparql")
       graph = @sparql.perform_sparql_transformation(graph, "fix_wikidata_uri.sparql")
       graph = @sparql.perform_sparql_transformation(graph, "fix_isni.sparql")
       graph = @sparql.perform_sparql_transformation(graph, "collapse_duplicate_contact_pointblanknodes.sparql")
