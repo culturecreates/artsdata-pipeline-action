1b4f63ea7f6d734e551652b9f262e28c3c9833f2
suhail-aliyar <suhail@culturecreates.com>
2026-02-09 12:49:35 +0400

    #53 Removed puts from test

--- test/sparql/add_schema_url_test.rb
+++ test/sparql/add_schema_url_test.rb
@@ -12,9 +12,7 @@ class AddUrlTest < Minitest::Test
     sparql_file = sparql_file.gsub("subject_url", "www.example-url.com")
     sparql = SPARQL.parse(sparql_file, update: true)
     graph = RDF::Graph.load("./test/fixtures/test_add_url.jsonld")
-    puts "before: #{graph.dump(:jsonld)}"
     graph.query(sparql)
-    puts "after: #{graph.dump(:jsonld)}"
     assert_equal(RDF::URI("www.example-url.com"),
                  graph.query([nil, RDF::URI("http://schema.org/url"), nil]).each.objects.first)
   end
@@ -24,9 +22,7 @@ class AddUrlTest < Minitest::Test
     sparql_file = sparql_file.gsub("subject_url", "www.example-new-url.com")
     sparql = SPARQL.parse(sparql_file, update: true)
     graph = RDF::Graph.load("./test/fixtures/test_event_with_url.jsonld")
-    puts "before: #{graph.dump(:jsonld)}"
     graph.query(sparql)
-    puts "after: #{graph.dump(:jsonld)}"
     assert_equal(RDF::URI("http://www.example-url.com"),
                  graph.query([nil, RDF::URI("http://schema.org/url"), nil]).each.objects.first)
   end
