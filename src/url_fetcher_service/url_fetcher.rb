25f794cbc1ca635f07ce7319b2b1cd9f6c1ef6f0
devnandan <devnandan@culturecreates.com>
2026-08-27 12:39:46 +0530

    fix : push the xml type check into the shared fetcher_with_retry

--- src/url_fetcher_service/url_fetcher.rb
+++ src/url_fetcher_service/url_fetcher.rb
@@ -21,7 +21,10 @@ module UrlFetcherService
           url = "#{page}#{page_number}"
           puts "Fetching entity urls from #{url}"
           accept = url.end_with?('.xml') ? "text/xml, application/xml, */*" : nil
-          page_data, content_type = @page_fetcher.fetcher_with_retry(page_url: url, selector: identifier, accept: accept)
+          expected_content_type = url.end_with?('.xml') ? 'xml' : nil
+          page_data, content_type = @page_fetcher.fetcher_with_retry(
+            page_url: url, selector: identifier, accept: accept, expected_content_type: expected_content_type
+          ) 
           if !page_data.nil?
             @atleast_one_page_loaded = true
           end
