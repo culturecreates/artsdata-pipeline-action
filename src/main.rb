18801aa06681170994230658db95d87e85f0e331
devnandan <devnandan@culturecreates.com>
2026-09-07 15:22:03 +0530

    fix : increased Limit to 150 URLs for testing

--- src/main.rb
+++ src/main.rb
@@ -175,7 +175,7 @@ if mode.include?('fetch')
     )
 
     if mode.include?('test')
-      entity_urls = entity_urls.take(40) # Limit to 40 URLs for testing
+      entity_urls = entity_urls.take(150) # Limit to 150 URLs for testing
     end
 
     notification_instance.send_notification(
